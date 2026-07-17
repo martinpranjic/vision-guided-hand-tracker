4a46653b927f14220ef979f9fa4a457dcdf04c2f
Martin Pranjic <din@email.com>
2026-07-17 14:23:00 +0200

    Add live camera and quit command

--- src/camera_test.py
+++ src/camera_test.py
@@ -5,10 +5,17 @@ camera = cv2.VideoCapture(0)
 if not camera.isOpened():
     raise RuntimeError("Could not open camera.")
 
-success, frame = camera.read()
+while True:
+    success, frame = camera.read()
+
+    if not success:
+        break
+
+    cv2.imshow("Camera", frame)
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+
 camera.release()
-
-if not success:
-    raise RuntimeError("Could not capture a frame.")
-
-print(frame.shape)
+cv2.destroyAllWindows()
